705742ed6142bafd136b6d87bda7b596c54189b1
Abhipank <102601915+Abhipank@users.noreply.github.com>
2022-04-04 11:54:04 +0530

    Add files via upload

---
+++ folder assignment 1/codes/tata.py
@@ -0,0 +1,32 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+x=[0, 1, 2, 3, 4, 5]
+y=[0, 0, 0, 0, 0, 0]
+
+
+
+plt.plot(x,y,'r*')
+
+
+
+
+for i,j in zip(x,y) :
+    plt.text(i,j,'({},{})'.format(i,j))
+	
+	#here plt can be directly below plt
+x=np.linspace(-6,6,100)# when it was bnefore zip line not plt text was shown may be due change of x
+
+plt.axvline(x=0,c="black",label='x=0')
+plt.axhline(y=0,c="black",label='y=0')
+#plt.axvline(x=-2,c='black',label='x=-2')
+#plt.axvline(x=5,c='black',label='x=5')
+
+#plt.plot(x,11*x-4,'-g',label='y=11*x-4')
+#plt.plot(x,15*x+4,'-b',label='y=15*x+4')
+#plt.plot(x,13*x+14,'-r',label='y=13*x+14')
+
+plt.legend()
+plt.grid()
+plt.show()
