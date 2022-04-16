2b21b2fc24e871e67caa96d1772e2cd8b30625e3
Abhipank <102601915+Abhipank@users.noreply.github.com>
2022-04-17 00:14:20 +0530

    Update g(x).py

--- folder assignment 2/codes/g(x).py
+++ folder assignment 2/codes/g(x).py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-coordinates = np.loadtxt('f(x).dat', dtype='float')
+coordinates = np.loadtxt('g(x).dat', dtype='float')
 X = []
 Y = []
 for coordinate in coordinates:
@@ -11,7 +11,7 @@ for coordinate in coordinates:
 
 x= np.linspace(-6, 6, 100)
 
-plt.plot(X,Y, label='$f(x)=2x^2+1$')
+plt.plot(X,Y, label='$g(x)=2x^2+1$')
 
 plt.axvline(x=0,c="black",linestyle='--',label="x-axis")
 plt.axhline(y=0,c="black",linestyle='--',label="y-axis")
