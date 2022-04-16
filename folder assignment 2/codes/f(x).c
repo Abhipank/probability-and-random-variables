0cb63bb47ad8273b1f2fea8033c5e78077beddce
Abhipank <102601915+Abhipank@users.noreply.github.com>
2022-04-17 00:26:27 +0530

    Add files via upload

---
+++ folder assignment 2/codes/f(x).c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+float f(float x)
+{
+    return x*x*x;
+}
+int main()
+{
+FILE*ptr=fopen("f(x).dat","w");
+float i;
+
+for(i=-6;i<=6;i+=0.01)
+{
+    fprintf(ptr,"%f %f\n",i,f(i));
+}
+   fclose(ptr);
+    return 0;
+}
