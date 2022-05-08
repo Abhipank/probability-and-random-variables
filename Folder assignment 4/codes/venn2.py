0f64a16fff335d0f6b8ee701deb712b2894a737c
Abhipank <102601915+Abhipank@users.noreply.github.com>
2022-05-08 22:07:46 +0530

    Update venn2.py

--- Folder assignment 4/codes/venn2.py
+++ Folder assignment 4/codes/venn2.py
@@ -4,7 +4,6 @@ from matplotlib import pyplot as plt
 
 venn3(subsets=(100,100,40,100,40,40,20),set_labels=('Event A','Event B','Event C')) 
  
- #assuming all events equally probabale according to the probabality level in 11 class .so equal weightage 
  
  
 plt.show()
