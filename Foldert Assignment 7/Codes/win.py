b8b5ba6c864dbdd0779ad2480ae2b7eb4956a713
Abhipank <102601915+Abhipank@users.noreply.github.com>
2022-05-23 16:09:51 +0530

    Update win.py

--- Foldert Assignment 7/Codes/win.py
+++ Foldert Assignment 7/Codes/win.py
@@ -2,17 +2,17 @@
 import numpy as np
 #using python library to achieve binomial distribution probability
 from scipy.stats import binom
-
+#setting values
 m=int(input("Enter the value of m: "))
 n=int(input("Enter the value of n: "))
 p=float(input("Enter the value of p: "))
-q=float(input("Enter the value of q: "))
-
-lst=list(np.arange(1,n))
+q=1-p
+#creating a list of integers from 0 to n-1
+lst=list(np.arange(0,n))
 
 sum=0
-
+#finding the probability
 for x in lst:
-  sum=sum+binom.pmf(m-1,m+x-1,p)*q
+  sum=sum+binom.pmf(m-1,m+x-1,p)*p
 
 print(sum)
